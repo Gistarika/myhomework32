03aabf3306a452f5cceedd12bbb878472d31f12b
Gistarika <imron20142014@gmail.com>
2025-09-06 10:09:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let yosh = prompt("Ismingizni kiriting: ")
+
+let yoosh = prompt("Familiyani kiriting: ")
+
+let yooosh = prompt("Yoshingizni kiriting: ")
+
+let yoooosh = prompt("Yoqtirgan fanizni kiriting: ")
+
+let yooooosh = prompt("Yashidigan tumanizni kiriting: ")
+
+console.log('ism:', yosh)
+
+console.log('familiya:', yoosh)
+
+console.log('yosh:', yooosh)
+
+console.log('yoqtirgan fan:', yoooosh)
+
+console.log('yashash manzili:', yooooosh)
